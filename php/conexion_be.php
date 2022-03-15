572e8deaad881cd961ee53c57cbcc29d864e7cb5
FrankCarranza07 <frank7carranza64gmail.com>
2022-03-14 21:16:02 -0300

    Se agrega el Proyecto

---
+++ php/conexion_be.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+    /*
+    if($conexion){
+        echo 'Conectado exitosamente a la base de datos';
+
+    }else{
+        echo 'No se ha podido conectar a la bade de datos';
+    }
+    */
+
+    
+
+
+
+
+?>
---
+++ php/registro_usuario_be.php
@@ -0,0 +1,33 @@
+<?php
+
+    include 'conexion_be.php';
+
+    $nombre_completo = $_POST ['nombre_completo'];
+    $correo = $_POST['correo'];
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['contrasena'];
+
+    $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena) 
+              VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena')";
+
+    $ejecutar = mysqli_query($conexion, $query);
+
+    if($ejecutar){
+        echo '
+            <script>
+                alert("USUARIO INGRESADO EXITOSAMENTE");
+                window.location = "../index.php";
+            </script>
+        ';
+    }else{
+        echo '
+        <script>
+        alert("INTENTE NUEVAMENTE, USUARIO NO ALMACENADO");
+        window.location = "../index.php";
+    </script> 
+        ';
+    }
+
+    mysqli_close($conexion);
+
+?>
